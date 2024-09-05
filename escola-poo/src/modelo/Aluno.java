b7b8e766ccff620ad29c4e800c8e36681dbe2e5b
yasmin <142116724+yasminbonett@users.noreply.github.com>
2024-09-05 00:02:31 -0300

    Add files via upload

---
+++ escola-poo/src/modelo/Aluno.java
@@ -0,0 +1,43 @@
+package modelo;
+
+public class Aluno {
+    private String nome;
+    private String matricula;
+    private String curso;
+
+    public Aluno(String nome, String matricula, String curso){
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getMatricula(){
+        return matricula;
+    }
+
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+
+    public String getCurso(){
+        return curso;
+    }
+
+    public void setCurso(String curso){
+        this.curso = curso;
+    }
+
+    public void mostrarAluno(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Curso: " + curso);
+    }
+}
