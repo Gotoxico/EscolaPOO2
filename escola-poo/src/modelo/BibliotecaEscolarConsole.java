c299ccfb1468ceb8cc43d7474b193c3552579b8b
Julia Sa Eller <juliasaeller@Julias-MacBook-Air.local>
2024-11-27 01:32:15 -0300

    feat: add BibliotecaEscolarConsole

--- escola-poo/src/modelo/BibliotecaEscolarConsole.java
+++ escola-poo/src/modelo/BibliotecaEscolarConsole.java
@@ -1,8 +1,8 @@
 package modelo;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
+
 
 public class BibliotecaEscolarConsole{    //classe com funções de impressão relacionada apenas às classes de Biblioteca
 
@@ -17,13 +17,25 @@ public class BibliotecaEscolarConsole{    //classe com funções de impressão r
         + diaEmprestimo.getMonth() + "/" + diaEmprestimo.getYear() + "\nData da Devolucao: " 
         + diaDevolucao.getDayOfMonth() + "/" + diaDevolucao.getMonth() + "/" + diaDevolucao.getYear() + "\n");
     } 
+    
+    /**
+     * @Brief: Imprime as datas de início e fim de uma multa
+     * @Parameter: multa Objeto do tipo Multa contendo as informações das datas
+     */
+    public void imprimeDatasMulta(Multa multa){
+        LocalDate inicioMulta = multa.getInicioMulta();
+        LocalDate fimMulta = multa.getFimMulta();
+        System.out.println("Multa: inicio: " + inicioMulta.getDayOfMonth() + "/" 
+        + inicioMulta.getMonth() + "/" + inicioMulta.getYear() + " fim: " 
+        + fimMulta.getDayOfMonth() + "/" + fimMulta.getMonth() + "/" + fimMulta.getYear() + "\n");
+    }
 
     /**
      * @Brief: Imprime informações detalhadas de um livro (título, autor, gênero, ISBN)
      * @Parameter: livro Objeto do tipo Livro contendo as informações
      */
     public void imprimirInformacoesLivroComParametros(Livro livro){
-        System.out.println("\nTitulo: " + livro.getTitulo() + "\nAutor: " + livro.getAutor() + "\nGenero: " + livro.getGenero() + "\nISBN: " + livro.getISBN());
+        System.out.println("Titulo: " + livro.getTitulo() + "\nAutor: " + livro.getAutor() + "\nGenero: " + livro.getGenero() + "\nISBN: " + livro.getISBN() + "\nStatus: " + livro.getEmprestado());
     }
 
     /**
@@ -39,11 +51,14 @@ public class BibliotecaEscolarConsole{    //classe com funções de impressão r
      * @Parameter: biblioteca Objeto do tipo Biblioteca contendo o catálogo de livros
      */
     public void imprimirCatalogoDeLivros(Biblioteca biblioteca){
-        ArrayList<Livro> livros = biblioteca.getLivros();
+        ArrayList<Livro> livros = new ArrayList<>();
+        livros = biblioteca.getLivros();
         biblioteca.ordenaLivrosOrdemAlfabetica();
         System.out.println("\nCATÁLOGO: \n"); 
         for(Livro livro : livros){
+            System.out.println("--");
             imprimirInformacoesLivroSemParametros(livro);
+            System.out.println("\n");
         }
     }
 
@@ -53,12 +68,15 @@ public class BibliotecaEscolarConsole{    //classe com funções de impressão r
      * @Parameter: genero Gênero desejado para filtrar os livros
      */
     public void imprimirLivrosDeGeneroEspecifico(Biblioteca biblioteca, String genero){
-        ArrayList<Livro> livros = biblioteca.getLivros();
+        ArrayList<Livro> livros = new ArrayList<>();
+        livros = biblioteca.getLivros();
         biblioteca.ordenaLivrosOrdemAlfabetica();
         System.out.println("\nLIVROS DE " + genero.toUpperCase() + ":\n");
         for(Livro livro : livros){
             if(livro.getGenero().equals(genero)){
+                System.out.println("--");
                 imprimirInformacoesLivroSemParametros(livro);
+                System.out.println("\n");
             }
         }
     }
@@ -71,8 +89,93 @@ public class BibliotecaEscolarConsole{    //classe com funções de impressão r
     public void imprimirRecomenadacoes(BibliotecaEscolar biblioteca, String disciplina){
         ArrayList<LivroDidatico> recomendacoes = biblioteca.recomendacoes(disciplina);
         for(LivroDidatico livro : recomendacoes){
+            System.out.println("--");
             imprimirInformacoesLivroSemParametros(livro);
+            System.out.println("\n");
         }
     }
 
+    /**
+     * @Brief: Imprime relatório geral de livros da biblioteca
+     * @Parameter: biblioteca Objeto do tipo Biblioteca contendo o vetor de livros
+     */
+    public void relatorioGeralLivros(Biblioteca biblioteca){
+        ArrayList<Livro> livros = biblioteca.getLivros();
+        System.out.println("\nRELATORIO GERAL: \n\n"); 
+        for(Livro livro : livros){
+            System.out.println("- ");
+            imprimirInformacoesLivroComParametros(livro);
+            System.out.println("\n");
+        }
+    }
+
+    /**
+     * @Brief: Imprime relatório geral de usuários multados e suas multas da biblioteca
+     * @Parameter: biblioteca Objeto do tipo Biblioteca contendo o vetor de usuários
+     */
+    public void relatorioMultas(Biblioteca biblioteca){
+        ArrayList<Usuario> usuarios = biblioteca.getUsuarios();
+        System.out.println("\nRELATORIO DE MULTAS: \n\n");
+        for(Usuario usuario : usuarios){
+            ArrayList<Multa> multas = usuario.getMultas();
+            if(multas.isEmpty() == false)
+                System.out.println("- " + usuario.getNome());
+                for(Multa multa : multas){
+                    imprimeDatasMulta(multa);
+                    System.out.println("\n");
+                }
+        }
+    }
+    
+    /**
+     * @Brief: Imprime resultado da busca de um livro da biblioteca pelo autor
+     * @Parameter: biblioteca Objeto do tipo Biblioteca contendo o vetor de livros
+     * @Parameter: autor Objeto do tipo String que indica o autor do livro que se deseja buscar
+     */
+    public void buscarLivroPorAutor(Biblioteca biblioteca, String autor){
+        if(biblioteca.buscarLivroPorAutor(autor) == true){
+            System.out.println("\nEste livro está na biblioteca!\n");
+        } else {
+            System.out.println("\nEste livro não está na biblioteca\n");
+        }
+    }
+
+    /**
+     * @Brief: Imprime resultado da busca de um livro da biblioteca pelo título
+     * @Parameter: biblioteca Objeto do tipo Biblioteca contendo o vetor de livros
+     * @Parameter: titulo Objeto do tipo String que indica o titulo do livro que se deseja buscar
+     */
+    public void buscarLivroPorTitulo(Biblioteca biblioteca, String titulo){
+        if(biblioteca.buscarLivroPorTitulo(titulo) == true){
+            System.out.println("\nEste livro está na biblioteca!\n");
+        } else {
+            System.out.println("\nEste livro não está na biblioteca\n");
+        }
+    }
+
+    /**
+     * @Brief: Imprime resultado da busca de um livro da biblioteca pelo ISBN
+     * @Parameter: biblioteca Objeto do tipo Biblioteca contendo o vetor de livros
+     * @Parameter: ISBN Objeto do tipo String que indica o ISBN do livro que se deseja buscar
+     */
+    public void buscarLivroPorISBN(Biblioteca biblioteca, String ISBN){
+        if(biblioteca.buscarLivroPorISBN(ISBN) == true){
+            System.out.println("\nEste livro está na biblioteca!\n");
+        } else {
+            System.out.println("\nEste livro não está na biblioteca\n");
+        }
+    }
+
+    /**
+     * @Brief: Imprime resultado da busca de um usuário da biblioteca 
+     * @Parameter: biblioteca Objeto do tipo Biblioteca contendo o vetor de usuários
+     * @Parameter: usuarioId Objeto do tipo String que indica o ID do usuário que se deseja buscar
+     */
+    public void buscarUsuario(Biblioteca biblioteca, String usuarioId){
+        if(biblioteca.buscarUsuario(usuarioId) == true){
+            System.out.println("\nEste usuário está cadastrado na biblioteca!\n");
+        } else {
+            System.out.println("\nEste usuário não está cadastrado na biblioteca\n");
+        }
+    }
 }
