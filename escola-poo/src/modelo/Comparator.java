47ebaf774c76b7d427027bbd70cf7e6e60db11da
Julia Sa Eller <juliasaeller@Julias-MacBook-Air.local>
2024-11-27 01:33:02 -0300

    feat: add Comparator

--- escola-poo/src/modelo/Comparator.java
+++ escola-poo/src/modelo/Comparator.java
@@ -1,5 +1,8 @@
 package modelo;
 
+/**
+ * @Brief: Interface que define o que o Comparator de Livro deve implementar 
+ */
 public interface Comparator {
     int compare(Livro livro1, Livro livro2);
 } 
