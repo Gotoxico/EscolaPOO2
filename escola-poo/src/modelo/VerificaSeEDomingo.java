1737abe4e8b8226b7ab1ad746c2210a6d4654e54
Julia Sa Eller <juliasaeller@Julias-MacBook-Air.local>
2024-11-27 01:36:02 -0300

    feat: add VerificaSeEDomingo

--- escola-poo/src/modelo/VerificaSeEDomingo.java
+++ escola-poo/src/modelo/VerificaSeEDomingo.java
@@ -6,13 +6,14 @@ import java.time.LocalDate;
  * @Brief: Classe que verifica se o dia atual é domingo
  */
 public class VerificaSeEDomingo{
-    
+    private static LocalDate diaAtual;
+
     /**
      * @Brief: Metodo que verifica se o dia atual é domingo
      * @Return: Retorna true se o dia atual for domingo, caso contrário, retorna false
      */
     public static boolean verificaSeEDomingo(){
-        LocalDate diaAtual = LocalDate.now();
+        diaAtual = LocalDate.now();
         if(diaAtual.getDayOfWeek().equals(DayOfWeek.SUNDAY)){  
             return true;
         }
